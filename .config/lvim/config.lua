3f2d7010d3ec1ef35ea447436660b3d47afcfe91
Dimitri Wahyudiputra <gitmitri@gmail.com>
2022-06-14 03:23:21 +0700

    lvim config 1.1.3

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,38 +1,55 @@
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "gruvbox"
+lvim.format_on_save = false
+lvim.colorscheme = "tokyonight"
 
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.clipboard = ""
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.showtabline = 0 -- hide tabs
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
+-- Copy
+vim.cmd("vnoremap \\c \"+y")
+vim.cmd("nnoremap \\c \"+y")
+
+-- Cut
+vim.cmd("vnoremap \\x \"+d")
+vim.cmd("nnoremap \\x \"+d")
+
+-- Paste
+vim.cmd("nnoremap \\p \"+p")
+vim.cmd("nnoremap \\P \"+P")
+
 lvim.builtin.alpha.active = false
+lvim.builtin.bufferline.active = false
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.cmp.experimental.ghost_text = false
 lvim.builtin.cmp.completion.autocomplete = false
 lvim.builtin.cmp.completion.completeopt = 'menu,menuone,noinsert'
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -40,46 +57,19 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    {
-        -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-        command = "prettier",
-        ---@usage arguments to pass to the formatter
-        -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-        extra_args = {},
-        ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-    },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    extra_args = {},
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  },
 }
 
 -- Additional Plugins
 lvim.plugins = {
-    { "whatyouhide/vim-gotham" },
-    { "morhetz/gruvbox" },
-    {
-        "aca/emmet-ls",
-        config = function()
-            local lspconfig = require("lspconfig")
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
-            capabilities.textDocument.completion.completionItem.resolveSupport = {
-                properties = {
-                    "documentation",
-                    "detail",
-                    "additionalTextEdits",
-                },
-            }
-
-            lspconfig.emmet_ls.setup({ capabilities = capabilities, filetypes = { "html",
-                "css",
-                "javascript",
-                "typescript",
-                "eruby",
-                "typescriptreact",
-                "javascriptreact",
-                "svelte",
-                "vue",
-            } })
-        end,
-    },
+  { "morhetz/gruvbox" },
+  { "folke/tokyonight.nvim" }
 }
