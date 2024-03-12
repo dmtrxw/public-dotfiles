251c06de1abd338ba77e9a93cfd141acb7329cd8
Dimitri <gitmitri@gmail.com>
2024-03-13 06:36:50 +0700

    Update configs

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -10,6 +10,7 @@ vim.opt.splitbelow = true -- force all horizontal splits to go below current win
 vim.opt.splitright = true -- force all vertical splits to go to the right of current window
 vim.opt.showtabline = 0   -- hide tabs
 vim.opt.relativenumber = true
+vim.diagnostic.config({ virtual_text = false })
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -88,7 +89,7 @@ require("presence").setup({
     git_commit_text     = "Committing changes",        -- Format string rendered when committing changes in git (either string or function(filename: string): string)
     plugin_manager_text = "Managing plugins",          -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
     reading_text        = "Reading a file",            -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-    workspace_text      = "Working on something cool", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    workspace_text      = "Working on something", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
     line_number_text    = "Line %s out of %s",         -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 })
 
